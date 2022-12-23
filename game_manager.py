84c9c4e92a00f1f2d45b8e0a373e8d2f8aa41619
dhsiehcode <89666559+dhsiehcode@users.noreply.github.com>
2022-12-23 16:54:09 +0800

    Checks surrounding 3 x 3 block for mines

--- game_manager.py
+++ game_manager.py
@@ -23,15 +23,37 @@ board = [['','','','','','','','',''],
 
 # returns if there are mines locally around i, j
 def local_has_mine(i, j):
+    # the four corners
+
+    if i == 0 and j == 0:
+        return board[i][j] == 'x' or board[i + 1][j] == 'x' or board[i][j + 1] == 'x' or board[i + 1][j + 1]
+    elif i == 0 and j == 8:
+        return board[i][j] == 'x' or board[i][j - 1] == 'x' or board[i + 1][j] == 'x' or board[i + 1][j - 1]
+    elif i == 8 and j == 0:
+        return board[i][j] == 'x' or board[i][j + 1] == 'x' or board[i - 1][j] == 'x' or board[i - 1][j + 1]
+    elif i == 8 and j == 8:
+        return  board[i][j] == 'x' or board[i - 1][j] == 'x' or board[i][j - 1] == 'x' or board[i - 1][j - 1]
+
+    # on the walls
     if i == 0:
-        if j == 0:
-            return board[i][j] == 'x' or 
+        return board[i][j - 1] == 'x' or board[i][j] == 'x' or board[i][j + 1] or board[i + 1][j - 1] == 'x' or \
+               board[i + 1][j] == 'x' or board[i + 1][j + 1]
     elif i == 8:
+        return board[i][j - 1] == 'x' or board[i][j] == 'x' or board[i][j + 1] or board[i - 1][j - 1] == 'x' or \
+               board[i - 1][j] == 'x' or board[i - 1][j + 1]
 
-
-    if j == 0:
-
+    elif j == 0:
+        return board[i - 1][j] == 'x' or board[i][j] == 'x' or board[i + 1][j] or board[i - 1][j + 1] == 'x' or \
+               board[i][j + 1] == 'x' or board[i + 1][j + 1]
     elif j == 8:
+        return board[i - 1][j] == 'x' or board[i][j] == 'x' or board[i + 1][j] or board[i + 1][j - 1] == 'x' or \
+               board[i][j - 1] == 'x' or board[i + 1][j - 1]
+
+    # all 8 corners
+    return board[i][j] == 'x' or board[i - 1][j - 1] == 'x' or board[i][j - 1] == 'x' or board[i + 1][j - 1] or \
+           board[i - 1][j] == 'x' or board[i + 1][j] or board[i - 1][j + 1] or board[i][j + 1] or board[i + 1][j + 1]
+
+
 
 
 def set_up():
