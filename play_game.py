f1196a19e7b4cafdf813800ecc6110562b22737f
dhsiehcode <89666559+dhsiehcode@users.noreply.github.com>
2022-12-26 22:18:40 +0800

    Completed method

--- play_game.py
+++ play_game.py
@@ -5,7 +5,13 @@ game_manager.set_up() # sets up board
 print('Here is the board: ')
 game_manager.display_playing_board()
 
-game_manager.f
+i = input('Please enter row (i value) of desired target: ')
+j = input('Please enter column (j value) of desired target: ')
+
+i = int(i)
+j = int(j)
+
+game_manager.first_move(i, j)
 
 while True:
 
